d976223a634fbfa158a091f9415bf578144f8222
ClickforceDev <clickforce01@gmail.com>
2023-11-07 00:15:12 +0800

    Initial commit

---
+++ ViewController.swift
@@ -0,0 +1,71 @@
+//
+//  ViewController.swift
+//  CFTMidRollSwiftSample
+//
+//  Created by CF-NB on 2023/11/6.
+//
+
+import UIKit
+import iMFADTV
+import AVKit
+
+var preroll: MFPreRollView?
+let kContentURLString: String = "https://v.holmesmind.com/1151/video/output/s_f96434d0f311f12bdcf5145796985719.mp4"
+var contentPlayerViewController = AVPlayerViewController.init()
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        setupContentPlayer()
+        DispatchQueue.main.asyncAfter(deadline: .now() + 5) {
+            self.setMidRoll()
+        }
+    }
+    
+    func setMidRoll(){
+        preroll = MFPreRollView.init(frame: self.view.frame)
+        preroll?.setZoneID("18379", get: self)
+        preroll?.delegate = self;
+        preroll?.setPlayer()
+    }
+    
+    func setupContentPlayer() {
+        let contentURL = URL(string: kContentURLString);
+        let player = AVPlayer(url: contentURL!);
+        contentPlayerViewController = AVPlayerViewController()
+        contentPlayerViewController.player = player
+        contentPlayerViewController.view.frame = self.view.bounds
+        
+        // Attach content video player to view hierarchy.
+        showContentPlayer()
+    }
+    
+    func showContentPlayer() {
+        self.addChild(contentPlayerViewController);
+        contentPlayerViewController.view.frame = self.view.bounds;
+        self.view.insertSubview(contentPlayerViewController.view, at: 0);
+        contentPlayerViewController.didMove(toParent: self);
+        contentPlayerViewController.player?.play()
+    }
+    
+    func hideContentPlayer() {
+        contentPlayerViewController.willMove(toParent: self);
+        contentPlayerViewController.view.removeFromSuperview();
+        contentPlayerViewController.removeFromParent()
+        contentPlayerViewController.player?.pause()
+    }
+}
+
+extension ViewController: MFPreRollDelegate {
+    func readyPlayVideo() {
+        print("MidRoll end, start you'r videp.")
+    }
+    
+    func onFailedToVast() {
+        print("MidRoll fail.")
+    }
+}
+
+
